526f84637c9dcb25f039add335d848bff94789bf
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-10-21 19:13:57 +0100

    new save

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>All the hells have been summoned and we are entering the void of uncertainty</h1>
+    </>
+  );
+}
+
+export default App;
