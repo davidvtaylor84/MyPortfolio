90829990c4b8055d323bbe8de0972784fa83b119
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-12-08 12:28:25 +0000

    created a email form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,33 +4,15 @@ import './Contact.css'
 
 const Contact = () => {
 
-
   return (
     <div className='contactContainer'>
-      <form onSubmit="sendEmail(); reset(); return false;">
-        <h3>Get in touch</h3>
-        <input type="text" id="name" placeholder='Your Name' required/>
-        <input type="email" id="email" placeholder='Email Address' required/>
-        <input type="text" id="phone" placeholder='Phone Number' required/>
-        <textarea id='message' rows='4' placeholder='How can I help?'></textarea>
+      <form target="_blank" action="https://formsubmit.co/davidvaldeztaylor@gmail.com" method="POST">
+        <h3>Get in touch via form or contact me at davidvaldeztaylor@gmail.com</h3>
+        <input type="text" name="name" placeholder='Your Name' required/>
+        <input type="email" name="email" placeholder='Email Address' required/>
+        <textarea name='message' rows='4' placeholder='How can I help?'></textarea>
         <button type='submit'>Send</button>
       </form>
-
-      {/* <script src="https://smtpjs.com/v3/smtp.js"></script>
-      <script>
-        function sendEmail(){
-          Email.send({
-            Host : "smtp.elasticemail.com",
-            Username : "username",
-            Password : "password",
-            To : 'them@website.com',
-            From : "you@isp.com",
-            Subject : "This is the subject",
-            Body : "And this is the body"
-        }).then(
-          message => alert(message)
-        )} */}
-      {/* </script> */}
     </div>
   )
 }
